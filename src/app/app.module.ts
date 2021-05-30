dc92419d23f9509e1b7952db88bb8bded026791b
Marcin Duda <geotix.md@gmail.com>
2021-05-30 22:36:36 +0200

    upload document change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,8 @@ import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
--- src/app/document-module/document-upload/document-upload.component.ts
+++ src/app/document-module/document-upload/document-upload.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-document-upload',
@@ -6,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-upload.component.scss']
 })
 export class DocumentUploadComponent implements OnInit {
+  fileName = '';
+  constructor(private http: HttpClient) {}
+  onFileSelected(event) {
 
-  constructor() { }
+    const file:File = event.target.files[0];
+
+    if (file) {
+
+        this.fileName = file.name;
+
+        const formData = new FormData();
+
+        formData.append("thumbnail", file);
+
+        const upload$ = this.http.post("/api/thumbnail-upload", formData);
+
+        upload$.subscribe();
+    }
+}
 
   ngOnInit(): void {
   }
